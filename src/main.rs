48c0ff3446e61bb45c632b671ed325a21dd8e08b
sachin maharana <sachin.nicky@gmail.com>
2020-04-11 19:47:47 +0530

    add routes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate actix_web;
+
+use actix_files as fs;
+use actix_web::http::StatusCode;
+use actix_web::{guard, middleware, web, App, HttpResponse, HttpServer, Result};
+
+use env_logger;
+
+use std::{env, io};
+
+#[get("/favicon")]
+async fn favicon() -> Result<fs::NamedFile> {
+    Ok(fs::NamedFile::open("static/favicon.ico")?)
+}
+#[get("/healthz")]
+async fn healthz() -> Result<HttpResponse> {
+    Ok(HttpResponse::Ok().json("Okay"))
+}
+
+async fn p404() -> Result<fs::NamedFile> {
+    Ok(fs::NamedFile::open("static/index.html")?.set_status_code(StatusCode::NOT_FOUND))
+}
+
+#[actix_rt::main]
+async fn main() -> io::Result<()> {
+    env::set_var("RUST_LOG", "actix_web=debug, actix_server=info");
+    env_logger::init();
+
+    HttpServer::new(|| {
+        App::new()
+            .wrap(middleware::Logger::default())
+            .service(favicon)
+            .service(healthz)
+            .default_service(
+                web::resource("").route(web::get().to(p404)).route(
+                    web::route()
+                        .guard(guard::Not(guard::Get()))
+                        .to(HttpResponse::MethodNotAllowed),
+                ),
+            )
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
